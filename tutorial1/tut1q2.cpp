e0e1ccad4b73fc20acbb9c71a64349bd288bc455
Ishika Walia <100034839+iishikawalia@users.noreply.github.com>
2025-08-18 08:34:56 +0530

    Update tut1q2.cpp

--- tutorial1/tut1q2.cpp
+++ tutorial1/tut1q2.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int array[] = {2, 3, 4, 5, 7, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 2, 4, 3, 5, 7, 8, 9, 8, 8};
+
+int pos = 0;
 int size = sizeof(array) / sizeof(array[0]);
+void Delete(int pos)
+{
+    for (int k = pos; k < size - 1; k++)
+    {
+        array[k] = array[k + 1];
+    }
+    size--;
+};
 
 void DeleteDuplicateElements()
 {
@@ -18,28 +28,23 @@ void DeleteDuplicateElements()
         }
     }
 };
-
 void display()
 {
     cout << "[";
-    for (int i = 0; i < size; i++)
+    for (int l = 0; l < size; l++)
     {
-        cout << array[i];
-        if (i < size - 1)
+        cout << array[l];
+        if (l < size - 1)
         {
             cout << ", ";
         }
     }
     cout << "]" << endl;
-};
-
+}
 int main()
 {
     cout << "Original Array: " << endl;
     display();
     DeleteDuplicateElements();
     display();
-
 }
-
-
